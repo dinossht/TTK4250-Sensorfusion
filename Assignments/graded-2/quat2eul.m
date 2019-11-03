8fdb19e7337c8213182fccbb7b68c3b66a7202c8
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2019-11-03 19:09:48 +0100

    make quat2eul work for n dimensional quaternions

--- Assignments/graded-2/quat2eul.m
+++ Assignments/graded-2/quat2eul.m
@@ -2,9 +2,9 @@ function eul = quat2eul(q)
 % Equation (10.38) & (10.39)
     qSquared = q.^2;
 
-    phi = atan2(2*(q(4,:)*q(3,:)+q(1,:)*q(2,:)), qSquared(1,:)-qSquared(2,:)-qSquared(3,:)+qSquared(4,:));
-    theta = asin(2*(q(1,:)*q(3,:)-q(2,:)*q(4,:)));
-    psi = atan2(2*(q(2,:)*q(3,:)+q(1,:)*q(4,:)), qSquared(1,:)+qSquared(2,:)-qSquared(3,:)-qSquared(4,:));
+    phi = atan2(2*(q(4,:).*q(3,:)+q(1,:).*q(2,:)), qSquared(1,:)-qSquared(2,:)-qSquared(3,:)+qSquared(4,:));
+    theta = asin(2*(q(1,:).*q(3,:)-q(2,:).*q(4,:)));
+    psi = atan2(2*(q(2,:).*q(3,:)+q(1,:).*q(4,:)), qSquared(1,:)+qSquared(2,:)-qSquared(3,:)-qSquared(4,:));
 
     eul = [phi; theta; psi];
 end
