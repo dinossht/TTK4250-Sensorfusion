c3a2eddccc5e02df7dcc24689c4a57ac6e68f038
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2019-11-17 18:52:58 +0100

    Add better calc tuning

--- Assignments/graded-2/calculate_tuning_params.m
+++ Assignments/graded-2/calculate_tuning_params.m
@@ -1,7 +1,24 @@
-%clear; clc; close all;
+clear; clc; close all;
 load task_simulation.mat;
 dt = mean(diff(timeIMU));
 steps = size(zAcc,2);
+
+%% Measurement noise
+% GNSS Position  measurement
+p_std =  [0.300    0.300    0.508]'; % Measurement noise
+RGNSS = diag(p_std.^2);
+
+% accelerometer
+qA = 10*1.4101e-04; % accelerometer measurement noise covariance
+qAb = 1000*1.4101e-05; % accelerometer bias driving noise covariance
+pAcc = 1e-9;  % accelerometer bias reciprocal time constant
+
+qG = 1000*2.0552e-07; % gyro measurement noise covariance
+qGb = 5000*2.0552e-08;  % gyro bias driving noise covariance
+pGyro = 1e-9; % gyrp bias reciprocal time constant
+
+%% Estimator
+eskf = ESKF(qA, qG, qAb, qGb, pAcc, pGyro);
 
 %% Noise power calculation GNSS
 
@@ -11,13 +28,7 @@ for k = 1:N
     display(k);
     if  timeIMU(k) >= timeGNSS(GNSSk)
         [gnssErr(:,GNSSk),~] = eskf.innovationGNSS(xtrue(:,k), ones(15,15), zGNSS(:,GNSSk), RGNSS, leverarm);
-        GNSSk = GNSSk  + 1;
-        
-        % sanity check, remove for some minor speed
-        if any(any(~isfinite(Pest(:, :, k))))
-            error('not finite Pest at time %d',k)
-        end
-        
+        GNSSk = GNSSk  + 1;        
     end
 end
 
@@ -30,75 +41,84 @@ figure(17);
 clf;
 title('Measurement noise: calculated vs. ideal')
 subplot(311);
-plot(gnssErr(1,:)); hold on; plot(sqrt(gnssPower(1))*randn(900,1)); legend('calculated','ideal')
+plot(gnssErr(1,:)); hold on; plot(sqrt(gnssPower(1))*randn(900,1)); legend('calculated','ideal'); ylim([-1 1]);
 
 subplot(312);
-plot(gnssErr(2,:)); hold on; plot(sqrt(gnssPower(2))*randn(900,1));
+plot(gnssErr(2,:)); hold on; plot(sqrt(gnssPower(2))*randn(900,1));ylim([-1 1]);
 
 subplot(313);
-plot(gnssErr(3,:)); hold on; plot(sqrt(gnssPower(3))*randn(900,1));
+plot(gnssErr(3,:)); hold on; plot(sqrt(gnssPower(3))*randn(900,1));ylim([-1 1]);
 
 
 %% Noise power calculation IMU Accelaration
-
 N = 90000;
-for k = 3500:8500
+for k = 1:N-2
     display(k);
     
     vel1 = xtrue(4:6,k);
-    vel2 = xtrue(4:6,k+1);
+    vel2 = xtrue(4:6,k+2);
     R = quat2rotmat(xtrue(7:10,k));
-    bias(:,k) = xtrue(11:13,k);
+    bias_g(:,k) = xtrue(11:13,k);
     
-    accTrue(:,k) = inv(R)*((vel2-vel1)/(dt));
-    imuAccErr(:,k) = zAcc(:,k)-accTrue(:,k)-bias(:,k);
+    bias_a(:,k) = xtrue(14:16,k);
+    
+    accTrue(:,k) = inv(R)*((vel2-vel1)/(2*dt)-[0; 0; 9.82]);    
+    imuAccErr(:,k) = zAcc(:,k)-accTrue(:,k)-bias_g(:,k);
 end
-%%
-imuAccErr1 = [dtrend(zAcc(1,3500:8500)) dtrend(zAcc(1,35000:39000))];
-imuAccErr2 = [dtrend(zAcc(2,3500:8500)) dtrend(zAcc(2,33000:39000))];
-imuAccErr3 = [dtrend(zAcc(3,6000:9000)) dtrend(zAcc(3,40000:39000))];
 
-imuAccPower(1) = sum(imuAccErr1.^2/(length(imuAccErr1)-1));
-imuAccPower(2) = sum(imuAccErr2.^2/(length(imuAccErr2)-1));
-imuAccPower(3) = sum(imuAccErr3.^2/(length(imuAccErr3)-1));
+%%  Noise power calculation IMU Accelaration
+imuAccErr = accTrue - zAcc(:,2:end-1);
 
+imuAccErr(1,:) = imuAccErr(1,:) - movmean(imuAccErr(1,:),5);
+imuAccErr(2,:) = imuAccErr(2,:) - movmean(imuAccErr(2,:),5);
+imuAccErr(3,:) = imuAccErr(3,:) - movmean(imuAccErr(3,:),5);
 
+imuAccErr(1,:) = min(0.05, max(-0.05, imuAccErr(1,:)));
+imuAccErr(2,:) = min(0.05, max(-0.05, imuAccErr(2,:)));
+imuAccErr(3,:) = min(0.05, max(-0.05, imuAccErr(3,:)));
+
+imuAccPower(1) = sum(imuAccErr(1,:).^2/(length(imuAccErr(1,:))-1));
+imuAccPower(2) = sum(imuAccErr(2,:).^2/(length(imuAccErr(2,:))-1));
+imuAccPower(3) = sum(imuAccErr(3,:).^2/(length(imuAccErr(3,:))-1));
+
+display(imuAccPower);
+m = size(imuAccErr,2);
 figure(18);
 clf;
 title('Measurement noise: calculated vs. ideal')
 subplot(311);
-plot(imuAccErr1); hold on; plot(sqrt(imuAccPower(1))*randn(length(imuAccErr1),1)); legend('calculated','ideal')
+plot(imuAccErr(1,:)); hold on; plot(sqrt(imuAccPower(1))*randn(m,1)); legend('calculated','ideal'); ylim([-0.06 0.06]);
 
 subplot(312);
-plot(imuAccErr2); hold on; plot(sqrt(imuAccPower(2))*randn(length(imuAccErr2),1));
+plot(imuAccErr(2,:)); hold on; plot(sqrt(imuAccPower(2))*randn(m,1));legend('calculated','ideal'); ylim([-0.06 0.06]);
 
 subplot(313);
-plot(imuAccErr3); hold on; plot(sqrt(imuAccPower(3))*randn(length(imuAccErr3),1));
+plot(imuAccErr(3,:)); hold on; plot(sqrt(imuAccPower(3))*randn(m,1));legend('calculated','ideal'); ylim([-0.06 0.06]);
 
 
-%%
-imuGyroErr1 = [dtrend(zGyro(1,5000:9000)) dtrend(zGyro(1,65000:69000))];
-imuGyroErr2 = [dtrend(zGyro(2,6000:9000)) dtrend(zGyro(2,67000:69000))];
-imuGyroErr3 = [dtrend(zGyro(3,6000:9000)) dtrend(zGyro(3,66000:69000))];
+%%  Noise power calculation IMU Gyros
+imuGyroErr(1,:) = zGyro(1,:)-movmean(zGyro(1,:),5);
+imuGyroErr(2,:) = zGyro(2,:)-movmean(zGyro(2,:),5);
+imuGyroErr(3,:) = zGyro(3,:)-movmean(zGyro(3,:),5);
 
-imuGyroPower(1) = sum(imuGyroErr1.^2/(length(imuGyroErr1)-1));
-imuGyroPower(2) = sum(imuGyroErr2.^2/(length(imuGyroErr2)-1));
-imuGyroPower(3) = sum(imuGyroErr3.^2/(length(imuGyroErr3)-1));
+imuGyroErr(1,:) = min(2e-3, max(-2e-3, imuGyroErr(1,:)));
+imuGyroErr(2,:) = min(2e-3, max(-2e-3, imuGyroErr(2,:)));
+imuGyroErr(3,:) = min(2e-3, max(-2e-3, imuGyroErr(3,:)));
 
+imuGyroPower(1) = sum(imuGyroErr(1,:).^2/(length(imuGyroErr(1,:))-1));
+imuGyroPower(2) = sum(imuGyroErr(2,:).^2/(length(imuGyroErr(2,:))-1));
+imuGyroPower(3) = sum(imuGyroErr(3,:).^2/(length(imuGyroErr(3,:))-1));
 
-figure(18);
+display(imuGyroPower);
+m = size(imuGyroErr,2);
+figure(19);
 clf;
 title('Measurement noise: calculated vs. ideal')
 subplot(311);
-plot(imuGyroErr1); hold on; plot(sqrt(imuGyroPower(1))*randn(length(imuGyroErr1),1)); legend('calculated','ideal')
+plot(imuGyroErr(1,:)); hold on; plot(sqrt(imuGyroPower(1))*randn(m,1)); legend('calculated','ideal'); ylim([-2e-3 2e-3]);
 
 subplot(312);
-plot(imuGyroErr2); hold on; plot(sqrt(imuGyroPower(2))*randn(length(imuGyroErr2),1));
+plot(imuGyroErr(2,:)); hold on; plot(sqrt(imuGyroPower(2))*randn(m,1));legend('calculated','ideal'); ylim([-2e-3 2e-3]);
 
 subplot(313);
-plot(imuGyroErr3); hold on; plot(sqrt(imuGyroPower(3))*randn(length(imuGyroErr3),1));
-
-
-
-
-
+plot(imuGyroErr(3,:)); hold on; plot(sqrt(imuGyroPower(3))*randn(m,1));legend('calculated','ideal'); ylim([-2e-3 2e-3]);
